e37466f19905d5a936cc717efdbfbf181e91c695
JasmineO <jasmine.ortizetienne@gmail.com>
2021-11-27 00:21:53 -0600

    Adding Parter to About Us

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
+import About  from './AboutComponent';
 
 
 
@@ -46,6 +47,7 @@ class Main extends Component {
         );
     }
 
+
     return (
       <div>
         <Header />
@@ -53,6 +55,7 @@ class Main extends Component {
             <Route path='/home' component={HomePage} />
             <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
             <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+            <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
             <Route exact path='/contactus' component={Contact} />
             <Redirect to='/home' />
         </Switch>
