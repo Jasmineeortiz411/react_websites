c30795f321dc23f9420180a64392fe7798279a65
JasmineO <jasmine.ortizetienne@gmail.com>
2021-12-05 19:57:27 -0600

    Fetch from Server

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderCard({item, isLoading, errMess}) {
     if (isLoading) {
@@ -9,9 +10,9 @@ function RenderCard({item, isLoading, errMess}) {
     if (errMess) {
         return <h4>{errMess}</h4>;
     }
-    return(
+    return (
         <Card>
-            <CardImg src={item.image} alt={item.name} />
+            <CardImg src={baseUrl + item.image} alt={item.name} />
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
                 <CardText>{item.description}</CardText>
@@ -24,16 +25,21 @@ function Home(props) {
         <div className="container">
             <div className="row">
                 <div className="col-md m-1">
-                    <RenderCard item={props.campsite} />
-                </div>
-                <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />
-                </div>
-                <div className="col-md m-1">
-                    <RenderCard
+                    <RenderCard 
                         item={props.campsite}
                         isLoading={props.campsitesLoading}
                         errMess={props.campsitesErrMess}
+                    />
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard 
+                        item={props.promotion}
+                        isLoading={props.promotionLoading}
+                        errMess={props.promotionErrMess}
+                    />
+                </div>
+                <div className="col-md m-1">
+                    <RenderCard item={props.partner}
                     />
                 </div>
             </div>
